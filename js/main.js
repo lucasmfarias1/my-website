ac0e05a52d926c42f06efa14f79bb3fb08f9a09d
Lucas Manzano Farias <lucasmfarias1@gmail.com>
2019-04-02 21:16:27 -0300

    tweak the page switching mechanism

--- js/main.js
+++ js/main.js
@@ -1,4 +1,22 @@
-$(".nav__link").click(() => {
-  $(".stack").slideUp();
-  $(".stack").slideDown();
-})
+let currentPage = 'stack';
+
+const switchPage = (e) => {
+  if (e.target.className == 'nav__link nav__active') {
+    return;
+  }
+
+  $('.nav__link').unbind('click');
+  
+  const idPageToClose = $('.nav__active').data('target');
+  const idPageToOpen = $(event.target).data('target');
+  
+  $(`#${idPageToClose}`).slideUp('fast', () => {
+    $(`#${idPageToOpen}`).slideDown('fast', () => {
+      $(`#${idPageToClose}-link`).removeClass('nav__active');
+      $(`#${idPageToOpen}-link`).addClass('nav__active');
+      $(".nav__link").click(switchPage);
+    });
+  });
+}
+
+$(".nav__link").click(switchPage);
